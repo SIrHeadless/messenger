249b68e0daee96c967798b8e0f7d996db66da3aa
Besitzer <Besitzer@192.168.0.3>
2016-03-16 22:13:00 +0100

    Added Comment

--- messenger/src/main/java/org/reuter/javabrains/messenger/resources/InjectDemoResource.java
+++ messenger/src/main/java/org/reuter/javabrains/messenger/resources/InjectDemoResource.java
@@ -1,12 +1,16 @@
 package org.reuter.javabrains.messenger.resources;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 
 @Path("/injectdemo")
@@ -17,8 +21,17 @@ public class InjectDemoResource {
 	@GET
 	@Path("annotations")
 	public String getParamsUsingAnnotations(@MatrixParam("param") String mparam,
-										@HeaderParam("customHeaderValue") String header) {
-		return mparam + " " + header;
+										@HeaderParam("customHeaderValue") String header,
+										@CookieParam("name") String cookie) {
+		return mparam + " " + header + " " + cookie;
+	}
+	
+	@GET
+	@Path("context")
+	public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders headers) {
+		String path = uriInfo.getAbsolutePath().toString();
+		headers.getCookies().toString();
+		return "Path : " + path;
 	}
 
 }
